dd3f481bec27de6fab7936157c85d256ec1b8b13
manjunathsonju <maunta.sth@gmail.com>
2024-08-09 17:54:20 +0545

    Added again after violation resolve

--- admin/config.php
+++ admin/config.php
@@ -4,7 +4,10 @@ $hostname = "http://localhost/intuji-assignment";
 $conn = mysqli_connect("localhost", "user1", "password1", "manju_cms") or die("Connection failed : " . mysqli_connect_error());
 
 // Google API configuration 
-
+define('GOOGLE_CLIENT_ID', 'put_something_here');
+define('GOOGLE_CLIENT_SECRET', 'put_something_here');
+define('GOOGLE_OAUTH_SCOPE', 'https://www.googleapis.com/auth/calendar');
+define('REDIRECT_URI', 'http://localhost/intuji-assignment/admin');
 
 // Start session 
 if (!session_id()) session_start();
